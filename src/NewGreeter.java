9608a0842181e389415629ebf76e648d2ad90149
Gene Lee <leexgenex@gmail.com>
2018-02-16 11:54:30 -0600

    New Greeter

---
+++ src/NewGreeter.java
@@ -0,0 +1,12 @@
+
+public class NewGreeter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Greeter me = new Greeter();
+	System.out.println(me.toString());
+		
+	}
+
+}
